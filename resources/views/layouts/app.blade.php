d7941fd2cdfb9196fcaba46ad07bebdbb57ad2fa
neil aldrei <neilaldrei@icloud.com>
2019-02-10 15:16:03 +0800

    Removed 1 typo, Everything is fine. no big deal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">4
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <!-- FA Icons -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
